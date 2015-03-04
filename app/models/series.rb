4f646d6140414e84efa4dce78f987f95b04d6202
Jean Simon <chicjean@gmail.com>
2015-03-03 20:49:12 -0700

    Cleaned up style sheets

--- app/models/series.rb
+++ app/models/series.rb
@@ -2,9 +2,6 @@ class Series < ActiveRecord::Base
 	has_many :comics, dependent: :destroy
 
 	validates :name, presence: true, uniqueness: {case_sensitive: false }
-
-	#scope :alpha_sort, -> {order(name: :asc)}
-	#Not case insensitive
 	
 	def self.alpha_sort
 		order ("lower(name) ASC")
