f4d759efa04441600e86a5867c45300907ba3628
Jean Simon <chicjean@gmail.com>
2015-03-07 14:08:32 -0700

    updating migration for heroku

--- db/migrate/20150301004037_change_comic_number_from_string_to_interger.rb
+++ db/migrate/20150301004037_change_comic_number_from_string_to_interger.rb
@@ -1,9 +1,10 @@
 class ChangeComicNumberFromStringToInterger < ActiveRecord::Migration
   def up
-  	change_column :comics, :number,  :integer
+  	change_column :comics, :number, 'integer USING CAST(number AS integer)'
   end
 
   def down
   	change_column :comics, :number, :string
   end
+
 end
